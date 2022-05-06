9cf7d2d25fbd805b8fc71ec169b09ea03cf25af1
Shrubhi <varadeabhijeet@gmail.com>
2022-05-06 18:37:09 +0530

    Add project files.

---
+++ ConsoleApp1/datebaseConnection.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+class datebaseConnection
+{
+    private string datasource = "WAIANGDESK03";
+    private string database = "SampleStore";
+    bool r = false;
+    //private string username;
+    //private string password;
+    public string message;
+    public SqlConnection conn;
+
+    public datebaseConnection()
+    {
+        //datasource = ConsoleMessage.ReadLine<string>("Enter server Name");
+        //database = ConsoleMessage.ReadLine<string>("Enter Database Name");
+        Connection();
+        Console.WriteLine(message);
+    }
+    public void Connection()
+    {
+        Console.WriteLine("Getting Connection ...");
+        string connString = $"Data Source={datasource};Initial Catalog={database};Integrated Security=True";
+         conn = new SqlConnection(connString);
+        try
+        {
+            //open connection
+            conn.Open();
+            
+            message = "Connection successful!";
+            r = true;
+
+        }
+        catch (Exception e)
+        {
+            r = false;
+            message = "Error: " + e.Message;
+        }
+
+       
+    }
+
+
+}
---
+++ ConsoleApp1/datebaseoperation.cs
@@ -0,0 +1,126 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+class datebaseoperation
+{
+    public bool t = false;
+    datebaseConnection dp = new datebaseConnection();
+
+    public void displayAllRecord()
+    {
+        try
+        {
+            string s = new string('_', 80);
+            Console.WriteLine(s);
+            string sql1 = "SELECT name FROM sys.columns WHERE object_id = OBJECT_ID('production.brands')";
+            using (SqlCommand cmd = new SqlCommand(sql1, dp.conn))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    Console.Write(reader.GetString(0) + "\t" + "|");
+                }
+                Console.WriteLine();
+                Console.WriteLine(s);
+                reader.Close();
+            }
+            string sql = "select * from production.brands";
+            using (SqlCommand cmd = new SqlCommand(sql, dp.conn))
+            {
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    Console.WriteLine($"|{row.ItemArray[0].ToString(),5}|{row.ItemArray[1].ToString(),20}|");
+
+
+                }
+                Console.WriteLine(s);
+                t = true;
+                Console.ReadKey(t);
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+
+
+    }
+    public void insertRecord(string BrandName)
+    {
+        try
+        {
+            string s = new string('_', 80);
+            Console.WriteLine(s);
+            string sql1 = "INSERT INTO [production].[brands]([brand_name]) values (@name)";
+            using (SqlCommand cmd = new SqlCommand(sql1, dp.conn))
+            {
+                cmd.Parameters.AddWithValue("@Name", BrandName);
+                int rows = cmd.ExecuteNonQuery();
+                Console.WriteLine("recorder Inserted .");
+                Console.WriteLine(rows);
+            }
+
+
+        }
+
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+
+
+    }
+
+
+
+
+
+    public void insertusingsp(string BrandName)
+    {
+        try
+        {
+            string s = new string('_', 80);
+            Console.WriteLine(s);
+            string sql1 = "proc_insert";
+            using (SqlCommand cmd = new SqlCommand(sql1, dp.conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@BrandName", BrandName);
+                int rows = cmd.ExecuteNonQuery();
+                Console.WriteLine("recorder Inserted .");
+                Console.WriteLine(rows);
+            }
+
+
+            t = true;
+
+            Console.ReadKey(t);
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+
+
+    }
+
+
+
+}
